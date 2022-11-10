c2ff7459ba1ee3771dcd18f70fe2bcc6a354075a
Mohamed Abaker <mabaker@vagelos-ve703-12805.apn.wlan.private.upenn.edu>
2022-11-10 16:28:38 -0500

    publish

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 
 const { v4: uuidv4 } = require('uuid');
 
-console.log('Hello World: ' + uuidv4())
+console.log('Hello World: ' + uuidv4());
 
-console.log("Penn Clubs is the best team in Labs");
+console.log('Penn Clubs is the best team in Labs');
